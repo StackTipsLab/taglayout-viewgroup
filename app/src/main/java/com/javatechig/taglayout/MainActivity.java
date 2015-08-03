3aee964edf2dd9064859e4560899b47e557a06bc
Nilanchala Panigrahy <nilanchalapanigrahy@gmail.com>
2015-08-03 16:09:28 +0100

    Initial commit

---
+++ app/src/main/java/com/javatechig/taglayout/MainActivity.java
@@ -0,0 +1,30 @@
+package com.javatechig.taglayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TagLayout tagLayout = (TagLayout) findViewById(R.id.tagLayout);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        String tag;
+        for (int i = 0; i <= 20; i++) {
+            tag = "#tag" + i;
+            View tagView = layoutInflater.inflate(R.layout.tag_layout, null, false);
+
+            TextView tagTextView = (TextView) tagView.findViewById(R.id.tagTextView);
+            tagTextView.setText(tag);
+            tagLayout.addView(tagView);
+        }
+    }
+}
---
+++ app/src/main/java/com/javatechig/taglayout/TagLayout.java
@@ -0,0 +1,67 @@
+package com.javatechig.taglayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Nilanchala on 8/3/15.
+ */
+public class TagLayout extends ViewGroup {
+
+    public TagLayout(Context context) {
+        super(context);
+    }
+
+    public TagLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TagLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        final int count = getChildCount();
+        int curWidth, curHeight, curLeft, curTop, maxHeight;
+
+        //get the available size of child view
+        final int childLeft = this.getPaddingLeft();
+        final int childTop = this.getPaddingTop();
+
+        final int childRight = this.getMeasuredWidth() - this.getPaddingRight();
+        final int childBottom = this.getMeasuredHeight() - this.getPaddingBottom();
+
+        final int childWidth = childRight - childLeft;
+        final int childHeight = childBottom - childTop;
+
+        maxHeight = 0;
+        curLeft = childLeft;
+        curTop = childTop;
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() == GONE)
+                return;
+
+            //Get the maximum size of the child
+            child.measure(MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.AT_MOST));
+            curWidth = child.getMeasuredWidth();
+            curHeight = child.getMeasuredHeight();
+            //wrap is reach to the end
+            if (curLeft + curWidth >= childRight) {
+                curLeft = childLeft;
+                curTop += maxHeight;
+                maxHeight = 0;
+            }
+            //do the layout
+            child.layout(curLeft, curTop, curLeft + curWidth, curTop + curHeight);
+            //store the max height
+            if (maxHeight < curHeight)
+                maxHeight = curHeight;
+            curLeft += curWidth;
+        }
+    }
+}
